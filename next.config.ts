2691f3b1a95158c3e37fe28797a4730b8b98821e
Juan Leonardo Ramirez Velasquez <juanlov4321@gmail.com>
2025-06-23 23:28:46 -0500

    🔧: update next.config.ts to include environment variables and remote image patterns
    📦: update package.json to add new dependencies and adjust existing ones

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	env: {
+		NEXT_PROJECT_ID: process.env.NEXT_PROJECT_ID,
+	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "**",
+
+				pathname: "**",
+			},
+		],
+	},
 };
 
 export default nextConfig;
