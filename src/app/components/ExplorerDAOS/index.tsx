2d35416b9aaa03b8a8932cc0b66462f5b97eb27f
Juan Leonardo Ramirez Velasquez <juanlov4321@gmail.com>
2025-06-23 23:31:04 -0500

    ✨ add ExplorerDAOS component to integrate HeroLanding and BoxDAOS

---
+++ src/app/components/ExplorerDAOS/index.tsx
@@ -0,0 +1,12 @@
+import HeroLanding from "../HeroLanding";
+import BoxDAOS from "./BoxDAOS";
+const ExplorerDAOS = () => {
+	return (
+		<>
+			<HeroLanding />
+			<BoxDAOS />
+		</>
+	);
+};
+
+export default ExplorerDAOS;
