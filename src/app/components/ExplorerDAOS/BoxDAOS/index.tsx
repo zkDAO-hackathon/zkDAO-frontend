10174bbd1f56cb65a2651ce716fb5445f26041c3
Juan Leonardo Ramirez Velasquez <juanlov4321@gmail.com>
2025-06-23 23:31:08 -0500

    ✨ add BoxDAOS component for DAO exploration and search functionality

---
+++ src/app/components/ExplorerDAOS/BoxDAOS/index.tsx
@@ -0,0 +1,69 @@
+"use client";
+import { useState, useEffect } from "react";
+import { FaSearch } from "react-icons/fa";
+import { FaPlus } from "react-icons/fa6";
+import { useAccount } from "wagmi";
+import CardDAO from "../CardDAO";
+import { DATACARDDAO } from "@/app/data/example.data";
+
+const BoxDAOS = () => {
+	const { isConnected: isAccountConnected } = useAccount();
+	const [searchQuery, setSearchQuery] = useState("");
+	const [debouncedSearchQuery, setDebouncedSearchQuery] = useState("");
+
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			setDebouncedSearchQuery(searchQuery);
+		}, 500);
+
+		return () => {
+			clearTimeout(timer);
+		};
+	}, [searchQuery]);
+
+	useEffect(() => {
+		if (debouncedSearchQuery) {
+			console.log("Searching for:", debouncedSearchQuery);
+		}
+	}, [debouncedSearchQuery]);
+
+	const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setSearchQuery(event.target.value);
+	};
+
+	return (
+		<div className='mx-auto container px-4 py-8'>
+			<h2 className='text-3xl font-bold mb-4'>Explore</h2>
+			<div className='flex items-center gap-4 mb-6'>
+				<label className='input input-border rounded-full w-full '>
+					<FaSearch />
+					<input type='text' value={searchQuery} onChange={handleSearchChange} placeholder='Search DAOs...' />
+				</label>
+				<button className='btn btn-primary rounded-full' disabled={!isAccountConnected}>
+					<FaPlus className='inline mr-2' />
+					Create DAO
+				</button>
+			</div>
+			<div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5'>
+				{DATACARDDAO.filter(
+					(dao) =>
+						dao.daoName.toLowerCase().includes(debouncedSearchQuery.toLowerCase()) ||
+						dao.daoDescription.toLowerCase().includes(debouncedSearchQuery.toLowerCase())
+				).map((dao, index) => (
+					<CardDAO
+						key={index}
+						daoName={dao.daoName}
+						daoDescription={dao.daoDescription}
+						daoAddresses={dao.daoAddresses}
+						daoLogo={dao.daoLogo}
+						daoChain={dao.daoChain}
+						daoChainId={dao.daoChainId}
+						daoId={dao.daoId}
+					/>
+				))}
+			</div>
+		</div>
+	);
+};
+
+export default BoxDAOS;
