1211a8f323fb39250d23f27aff93d329ab5b83f4
Juan Leonardo Ramirez Velasquez <juanlov4321@gmail.com>
2025-06-23 23:30:54 -0500

    ✨ add WalletContextProvider component for managing wallet state and context

---
+++ src/app/WalletContextProvider.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { wagmiAdapter, projectId } from "@/app/config";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createAppKit } from "@reown/appkit/react";
+import { mainnet, avalancheFuji } from "@reown/appkit/networks";
+import React, { type ReactNode } from "react";
+import { cookieToInitialState, WagmiProvider, type Config } from "wagmi";
+
+const queryClient = new QueryClient();
+
+if (!projectId) {
+	throw new Error("Project ID is not defined");
+}
+
+// Set up metadata
+const metadata = {
+	name: "zkDao",
+	description: "",
+	url: "https://appkitexampleapp.com",
+	icons: ["https://avatars.githubusercontent.com/u/179229932"],
+};
+
+// Create the modal
+export const modal = createAppKit({
+	adapters: [wagmiAdapter],
+	projectId,
+	networks: [mainnet, avalancheFuji],
+	defaultNetwork: avalancheFuji,
+	metadata: metadata,
+	features: {
+		analytics: false,
+	},
+	themeMode: "light",
+});
+
+function ContextProvider({ children, cookies }: { children: ReactNode; cookies: string | null }) {
+	const initialState = cookieToInitialState(wagmiAdapter.wagmiConfig as Config, cookies);
+
+	return (
+		<WagmiProvider config={wagmiAdapter.wagmiConfig as Config} initialState={initialState}>
+			<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+		</WagmiProvider>
+	);
+}
+
+export default ContextProvider;
