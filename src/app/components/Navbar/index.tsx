8c4e8d77310422ff45eab04024ef6b478a7a5ce9
Juan Leonardo Ramirez Velasquez <juanlov4321@gmail.com>
2025-06-24 22:47:42 -0500

    ✨ replace anchor tag with Link component for improved navigation in Navbar

--- src/app/components/Navbar/index.tsx
+++ src/app/components/Navbar/index.tsx
@@ -1,9 +1,12 @@
 import ConnectButton from "@/app/components/ConnectWalletButton";
+import Link from "next/link";
 const Navbar = () => {
 	return (
 		<div className='navbar sticky top-0 z-50 backdrop-blur-sm bg-white/10'>
 			<div className='flex-1'>
-				<a className='btn btn-ghost text-xl'>zkDAO</a>
+				<Link href={"/"} className='btn btn-ghost text-xl'>
+					zkDAO
+				</Link>
 			</div>
 			<div className='flex-none'>
 				<ConnectButton />
