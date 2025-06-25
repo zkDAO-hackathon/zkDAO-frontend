2d358eaac2dff1be91e8b0b82b224e9879adedb8
Juan Leonardo Ramirez Velasquez <juanlov4321@gmail.com>
2025-06-24 22:47:47 -0500

    ✨ refactor import statements and formatting in Wagmi Adapter configuration

--- src/app/config/index.tsx
+++ src/app/config/index.tsx
@@ -1,23 +1,23 @@
-import { cookieStorage, createStorage, http } from "@wagmi/core";
+import { cookieStorage, createStorage } from "@wagmi/core";
 import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
 import { mainnet, arbitrum } from "@reown/appkit/networks";
 
 export const projectId = process.env.NEXT_PROJECT_ID;
 
 if (!projectId) {
-  throw new Error("Project ID is not defined");
+	throw new Error("Project ID is not defined");
 }
 
 export const networks = [mainnet, arbitrum];
 
 //Set up the Wagmi Adapter (Config)
 export const wagmiAdapter = new WagmiAdapter({
-  storage: createStorage({
-    storage: cookieStorage,
-  }),
-  ssr: true,
-  projectId,
-  networks,
+	storage: createStorage({
+		storage: cookieStorage,
+	}),
+	ssr: true,
+	projectId,
+	networks,
 });
 
 export const config = wagmiAdapter.wagmiConfig;
