23794dc6ae86b9ec9d64eff2a31dcb6310a67db7
Juan Leonardo Ramirez Velasquez <juanlov4321@gmail.com>
2025-06-24 22:47:20 -0500

    ✨ add CreateDaoPage component with wallet connection check and view transition

---
+++ src/app/(pages)/create-dao/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+import CreateDaoForm from "@/app/components/CreateDaoForm";
+import Navbar from "@/app/components/Navbar";
+import { useAccount } from "wagmi";
+import Image from "next/image";
+import SVG404V2 from "@/app/assets/404-v2.svg";
+import { unstable_ViewTransition as ViewTransition } from "react";
+
+const CreateDaoPage = () => {
+	const { isConnected } = useAccount();
+
+	if (!isConnected) {
+		return (
+			<main>
+				<Navbar />
+				<div className='text-center mt-10'>
+					<Image src={SVG404V2} alt='Connect Wallet' width={500} height={150} className='mx-auto mb-4' />
+					<h1 className='text-2xl font-bold'>Please connect your wallet to create a DAO</h1>
+				</div>
+			</main>
+		);
+	}
+
+	return (
+		<>
+			<Navbar />
+			<ViewTransition name='page-create-dao'>
+				<CreateDaoForm />
+			</ViewTransition>
+		</>
+	);
+};
+
+export default CreateDaoPage;
