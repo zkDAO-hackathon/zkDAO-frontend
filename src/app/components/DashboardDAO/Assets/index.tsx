41a1532202348f2378e367667d76069f049cddf4
Juan Leonardo Ramirez Velasquez <juanlov4321@gmail.com>
2025-06-25 23:02:04 -0500

    ✨ add Assets component for displaying DAO-related assets

---
+++ src/app/components/DashboardDAO/Assets/index.tsx
@@ -0,0 +1,13 @@
+const Assets = () => {
+	return (
+		<div className='flex flex-col gap-4'>
+			<h2 className='text-2xl font-bold'>Assets</h2>
+			<p className='text-gray-600'>This section will display the assets associated with the DAO.</p>
+			{/* Placeholder for assets content */}
+			<div className='bg-white p-4 rounded shadow'>
+				<p>Assets content will be displayed here.</p>
+			</div>
+		</div>
+	);
+};
+export default Assets;
