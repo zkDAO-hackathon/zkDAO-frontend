52bb3d9cc98b3886eca877630d184fcc57493ce0
Juan Leonardo Ramirez Velasquez <juanlov4321@gmail.com>
2025-06-28 00:12:01 -0500

    ✨ add Footer component to Home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import ExplorerDAOS from "./components/ExplorerDAOS";
+import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
 		<>
 			<Navbar />
 			<ExplorerDAOS />
+			<Footer />
 		</>
 	);
 }
