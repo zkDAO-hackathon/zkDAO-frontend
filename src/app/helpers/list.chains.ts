19d6abf12db6183478fcec3534c05b10ccbf5891
Juan Leonardo Ramirez Velasquez <juanlov4321@gmail.com>
2025-06-28 00:13:36 -0500

    ✨ update CHAINS list to remove Ethereum Mainnet and Polygon, add Ethereum Sepolia, and fix isTestnet typos

--- src/app/helpers/list.chains.ts
+++ src/app/helpers/list.chains.ts
@@ -1,34 +1,22 @@
 export const CHAINS = [
 	{
-		id: "1",
-		name: "Ethereum Mainnet",
-		symbol: "ETH",
-		rpcUrl: "https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID",
-		blockExplorerUrl: "https://etherscan.io",
-		logo: "https://img.icons8.com/fluent/512/ethereum.png",
-		isTesnet: false,
-	},
-	{
-		id: "137",
-		name: "Polygon",
-		symbol: "MATIC",
-		rpcUrl: "https://polygon-rpc.com",
-		blockExplorerUrl: "https://polygonscan.com",
-		logo: "https://altcoinsbox.com/wp-content/uploads/2023/03/matic-logo.png",
-		isTesnet: false,
-	},
-
-	//avalanche
-	{
 		id: "43114",
 		name: "Avalanche C-Chain",
 		symbol: "AVAX",
 		rpcUrl: "https://api.avax.network/ext/bc/C/rpc",
 		blockExplorerUrl: "https://snowtrace.io",
 		logo: "https://upload.wikimedia.org/wikipedia/en/0/03/Avalanche_logo_without_text.png",
-		isTesnet: false,
+		isTestnet: false,
 	},
-	//avalanche testnet
+	{
+		id: "11155111",
+		name: "Ethereum Sepolia",
+		symbol: "ETH",
+		rpcUrl: "https://sepolia.infura.io/v3/YOUR_INFURA_PROJECT_ID",
+		blockExplorerUrl: "https://sepolia.etherscan.io",
+		logo: "https://img.icons8.com/fluent/512/ethereum.png",
+		isTestnet: true,
+	},
 	{
 		id: "43113",
 		name: "Avalanche Fuji Testnet",
@@ -36,6 +24,6 @@ export const CHAINS = [
 		rpcUrl: "https://api.avax-test.network/ext/bc/C/rpc",
 		blockExplorerUrl: "https://testnet.snowtrace.io",
 		logo: "https://upload.wikimedia.org/wikipedia/en/0/03/Avalanche_logo_without_text.png",
-		isTesnet: true,
+		isTestnet: true,
 	},
 ];
