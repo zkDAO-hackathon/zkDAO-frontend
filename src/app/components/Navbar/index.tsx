2f02b85a968ece8fe662be21cb57137eb62e73fc
Juan Leonardo Ramirez Velasquez <juanlov4321@gmail.com>
2025-06-28 00:13:27 -0500

    ✨ update Navbar component to use logo image and improve link styling

--- src/app/components/Navbar/index.tsx
+++ src/app/components/Navbar/index.tsx
@@ -1,11 +1,13 @@
 import ConnectButton from "@/app/components/ConnectWalletButton";
 import Link from "next/link";
+import LogoZKDAO from "@/app/assets/zkDAOLogo.svg";
+import Image from "next/image";
 const Navbar = () => {
 	return (
-		<div className='navbar sticky top-0 z-50 backdrop-blur-sm bg-white/10'>
+		<div className='navbar sticky top-0 z-50 backdrop-blur-sm'>
 			<div className='flex-1'>
-				<Link href={"/"} className='btn btn-ghost text-xl'>
-					zkDAO
+				<Link href='/' className='flex items-center px-2 transition-all hover:opacity-80' aria-label='ZKDAO Home'>
+					<Image src={LogoZKDAO} alt='ZKDAO Logo' className='w-auto h-12 md:h-[50px]' priority />
 				</Link>
 			</div>
 			<div className='flex-none'>
