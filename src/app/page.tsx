bc9879ab2ff31941ba38971c6930618b33cc7b38
Juan Leonardo Ramirez Velasquez <juanlov4321@gmail.com>
2025-06-28 23:14:17 -0500

    ✨ add spacing for improved layout in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import ExplorerDAOS from "./components/ExplorerDAOS";
+
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 
@@ -6,6 +7,7 @@ export default function Home() {
 	return (
 		<>
 			<Navbar />
+
 			<ExplorerDAOS />
 			<Footer />
 		</>
