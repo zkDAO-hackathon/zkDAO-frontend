ba32e695d0b1c96d854701de31e62991949130c0
Juan Leonardo Ramirez Velasquez <juanlov4321@gmail.com>
2025-06-28 23:14:53 -0500

    ✨ add QuequedProposal component to ExplorerDAOS for improved proposal visibility

--- src/app/components/ExplorerDAOS/index.tsx
+++ src/app/components/ExplorerDAOS/index.tsx
@@ -1,9 +1,11 @@
 import HeroLanding from "../HeroLanding";
 import BoxDAOS from "./BoxDAOS";
+import QuequedProposal from "./QuequedDAO";
 const ExplorerDAOS = () => {
 	return (
 		<>
 			<HeroLanding />
+			<QuequedProposal />
 			<BoxDAOS />
 		</>
 	);
