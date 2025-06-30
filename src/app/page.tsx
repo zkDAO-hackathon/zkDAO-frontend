9ef5e91468811504cc65a77d93d5dbad554e7b8d
Juan Leonardo Ramirez Velasquez <juanlov4321@gmail.com>
2025-06-29 22:06:53 -0500

    ✨ Refactor Home component for improved readability and maintainability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,48 +9,32 @@ import { useStore } from "@/app/store";
 import { useEffect } from "react";
 
 export default function Home() {
-  const { chainId } = useAccount();
-  const {
-    checkUpkeep,
-    fetchingCheckUpkeep,
-    daos,
-    fetchingDaos,
-    queuedProposals,
-    getCheckUpkeep,
-    getDaos,
-    setNetwork,
-  } = useStore();
+	const { chainId } = useAccount();
+	const { checkUpkeep, fetchingCheckUpkeep, daos, fetchingDaos, queuedProposals, getCheckUpkeep, getDaos, setNetwork } = useStore();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      if (daos.length === 0) {
-        await Promise.all([getCheckUpkeep(), getDaos()]);
-      }
-    };
+	useEffect(() => {
+		const fetchData = async () => {
+			if (daos.length === 0) {
+				await Promise.all([getCheckUpkeep(), getDaos()]);
+			}
+		};
 
-    if (chainId) {
-      setNetwork(chainId === 11155111 ? "sepolia" : "fuji");
-      fetchData();
-    }
-  }, [chainId]);
+		if (chainId) {
+			setNetwork(chainId === 11155111 ? "sepolia" : "fuji");
+			fetchData();
+		}
+	}, [chainId]);
 
-  if (fetchingDaos && fetchingCheckUpkeep) return <div>Loading DAOs...</div>;
+	if (fetchingDaos && fetchingCheckUpkeep) return <div>Loading DAOs...</div>;
 
-  if (daos.length !== 0)
-    console.log(
-      "DAOs:",
-      daos,
-      "Queued Proposals:",
-      queuedProposals,
-      "Check Upkeep:",
-      checkUpkeep
-    );
+	if (daos.length !== 0) console.log("DAOs:", daos, "Queued Proposals:", queuedProposals, "Check Upkeep:", checkUpkeep);
 
-  return (
-    <>
-      <Navbar />
-      <ExplorerDAOS />
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<Navbar />
+
+			<ExplorerDAOS />
+			<Footer />
+		</>
+	);
 }
