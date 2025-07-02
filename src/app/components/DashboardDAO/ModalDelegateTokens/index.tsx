f517c583b8e6901d34bbf02865749e8476404bcc
Juan Leonardo Ramirez Velasquez <juanlov4321@gmail.com>
2025-07-01 22:59:53 -0500

    ✨ Implement token delegation functionality in ModalDelegateTokens component

--- src/app/components/DashboardDAO/ModalDelegateTokens/index.tsx
+++ src/app/components/DashboardDAO/ModalDelegateTokens/index.tsx
@@ -1,6 +1,10 @@
 import Modal from "../../Modal";
 import { useForm } from "react-hook-form";
 import { IDelegateTokens } from "@/app/types/delegate.types";
+import { toast } from "sonner";
+import { useAccount, useWalletClient } from "wagmi";
+import { ZKDAO_JSON } from "@/app/config/const";
+import { GovernorContract } from "@/app/services/blockchain/contracts/governor";
 
 const ModalDelegateTokens = () => {
 	const {
@@ -8,17 +12,26 @@ const ModalDelegateTokens = () => {
 		handleSubmit,
 		formState: { errors },
 	} = useForm<IDelegateTokens>();
+
+	const { chainId } = useAccount();
+	const { data: walletClient } = useWalletClient();
+	const network = chainId === 11155111 ? "sepolia" : "avalancheFuji";
+	const governor = new GovernorContract(network, ZKDAO_JSON.address as `0x${string}`, walletClient);
+
+	const onSubmit = async (data: IDelegateTokens) => {
+		toast.promise(governor.delegateVotes(data.address as `0x${string}`), {
+			loading: "Delegating tokens...",
+			success: "Tokens delegated successfully",
+			error: "Error delegating tokens",
+		});
+	};
+
 	return (
 		<Modal id='delegate-modal'>
 			<h2 className='text-xl font-bold mb-2'>Delegate Tokens</h2>
 			<p className='text-gray-600 mb-4'>This feature allows you to delegate your tokens to a DAO.</p>
 
-			<form
-				onSubmit={handleSubmit((data) => {
-					console.log(data);
-					// TODO: Add actual delegation logic here
-				})}
-				className='flex flex-col gap-4'>
+			<form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-4'>
 				<div className='form-control'>
 					<label className='label'>
 						<span className='label-text'>Recipient Address</span>
