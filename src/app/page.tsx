20a099a4ca2bf013dc63b83427367467e874d105
Juan Leonardo Ramirez Velasquez <juanlov4321@gmail.com>
2025-07-06 13:10:49 -0500

    ✨ Add confetti effect on upkeep condition change and improve polling logic in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,47 @@ import ExplorerDAOS from "./components/ExplorerDAOS";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import { useStore } from "@/app/store";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import confetti from "canvas-confetti";
 
 export default function Home() {
 	const { chainId } = useAccount();
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	const { checkUpkeep, fetchingCheckUpkeep, daos, fetchingDaos, queuedProposals, getCheckUpkeep, getDaos, setNetwork } = useStore();
+
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	const [upkeepHistory, setUpkeepHistory] = useState([false, false]);
+
+	useEffect(() => {
+		const interval = setInterval(async () => {
+			try {
+				const res = await getCheckUpkeep();
+				console.log("Polling Check Upkeep:", res);
+
+				// Extract the condition from the response
+				const currentCondition = res?.performData !== "0x" && !res?.upkeepNeeded;
+
+				setUpkeepHistory((prevHistory) => {
+					const newHistory = [prevHistory[1], currentCondition];
+
+					// Check if we should trigger confetti
+					if (newHistory[0] === true && newHistory[1] === false) {
+						confetti({
+							particleCount: 200,
+							spread: 100,
+							origin: { y: 0.6 },
+						});
+					}
+
+					return newHistory;
+				});
+			} catch (err) {
+				console.error("Error al hacer polling", err);
+			}
+		}, 5000); // cada 5 segundos
+
+		return () => clearInterval(interval);
+	}, [getCheckUpkeep]);
 
 	useEffect(() => {
 		// Set default network if no wallet is connected
@@ -19,7 +54,7 @@ export default function Home() {
 
 		const fetchData = async () => {
 			if (daos.length === 0) {
-				await Promise.all([getCheckUpkeep(), getDaos()]);
+				await Promise.all([getDaos()]);
 			}
 		};
 
