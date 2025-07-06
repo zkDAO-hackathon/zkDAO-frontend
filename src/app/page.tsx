1a54725592cbdb9b6526a655f4684656b6b3b9b6
Juan Leonardo Ramirez Velasquez <juanlov4321@gmail.com>
2025-07-06 10:51:31 -0500

    ✨ Refactor Home component to streamline network setting and data fetching logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,50 +2,38 @@
 
 import { useAccount } from "wagmi";
 import ExplorerDAOS from "./components/ExplorerDAOS";
-
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import { useStore } from "@/app/store";
 import { useEffect } from "react";
-import Image from "next/image";
-import ZKDAO_LOGO from "@/app/assets/zkDAOLogo.svg";
 
 export default function Home() {
 	const { chainId } = useAccount();
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	const { checkUpkeep, fetchingCheckUpkeep, daos, fetchingDaos, queuedProposals, getCheckUpkeep, getDaos, setNetwork } = useStore();
 
 	useEffect(() => {
+		// Set default network if no wallet is connected
+		const network = chainId ? (chainId === 11155111 ? "sepolia" : "fuji") : "sepolia";
+		setNetwork(network);
+
 		const fetchData = async () => {
 			if (daos.length === 0) {
 				await Promise.all([getCheckUpkeep(), getDaos()]);
 			}
 		};
 
-		if (chainId) {
-			setNetwork(chainId === 11155111 ? "sepolia" : "fuji");
-			fetchData();
-		}
+		fetchData();
+
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [chainId]);
 
-	// if (fetchingDaos && fetchingCheckUpkeep) return <div>Loading DAOs...</div>;
-
-	if (daos.length !== 0) console.log("DAOs:", daos, "Queued Proposals:", queuedProposals, "Check Upkeep:", checkUpkeep);
+	// if (daos.length !== 0) console.log("DAOs:", daos, "Queued Proposals:", queuedProposals, "Check Upkeep:", checkUpkeep);
 
 	return (
 		<>
 			<Navbar />
-			{fetchingDaos && fetchingCheckUpkeep ? (
-				// skeleton loading
-				<div className='flex items-center justify-center h-screen flex-col space-y-4'>
-					<Image src={ZKDAO_LOGO} alt='zkDAO Logo' width={100} height={100} className='animate-pulse' />
-					{/* <span className='loading loading-infinity loading-xl'></span> */}
-					<span>Loading Daos...</span>
-				</div>
-			) : (
-				<ExplorerDAOS />
-			)}
-
+			<ExplorerDAOS />
 			<Footer />
 		</>
 	);
