8227d3193910671fb69f644bb3b2501af4a05dfe
Juan Leonardo Ramirez Velasquez <juanlov4321@gmail.com>
2025-07-05 19:21:54 -0500

    ✨ Enhance CardProposals component with user and time icons for improved readability

--- src/app/components/DashboardDAO/Proposals/CardProposals/index.tsx
+++ src/app/components/DashboardDAO/Proposals/CardProposals/index.tsx
@@ -1,5 +1,7 @@
 import { ProposalState } from "@/app/types/proposal.type";
 import { useEffect, useState } from "react";
+import { FaRegClock, FaUser, FaCalendarAlt } from "react-icons/fa";
+// import { BiTime } from 'react-icons/bi';
 interface CardProposalsProps {
 	status: number | string;
 	name: string;
@@ -53,7 +55,7 @@ const CardProposals = ({ status, name, description, by, timePublished, timeLeft 
 	return (
 		<div className='card bg-white shadow-md rounded-2xl p-6 hover:shadow-lg transition-shadow duration-300'>
 			<div className='flex justify-between items-start mb-3'>
-				<h2 className='font-bold text-lg text-gray-800'>{name}</h2>
+				<h2 className='font-bold text-lg text-gray-800 truncate max-w-[70%]'>{name}</h2>
 
 				<div className='flex items-center gap-2 text-sm text-white badge badge-neutral px-3 py-1.5'>
 					<div className='inline-grid *:[grid-area:1/1]'>
@@ -80,20 +82,24 @@ const CardProposals = ({ status, name, description, by, timePublished, timeLeft 
 
 			<p className='my-3 text-gray-700 line-clamp-3'>{description}</p>
 			<div className='mt-4 flex flex-col space-y-3 text-gray-500'>
-				<div className='font-medium'>
-					By: <span className='text-gray-700'>{`${by.substring(0, 6)}…${by.substring(by.length - 4)}`}</span>
+				<div className='font-medium flex items-center gap-2'>
+					<FaUser className='text-gray-400' />
+					<span>By:</span> <span className='text-gray-700'>{`${by.substring(0, 6)}…${by.substring(by.length - 4)}`}</span>
 				</div>
-				<div>
-					Published:{" "}
-					{new Date(timePublished).toLocaleString(undefined, {
-						year: "numeric",
-						month: "short",
-						day: "numeric",
-					})}
+				<div className='flex items-center gap-2'>
+					<FaCalendarAlt className='text-gray-400' />
+					<span>Published:</span>
+					<span>
+						{new Date(timePublished).toLocaleString(undefined, {
+							year: "numeric",
+							month: "short",
+							day: "numeric",
+						})}
+					</span>
 				</div>
 				<div className='flex items-center gap-2 text-sm text-gray-500'>
+					<FaRegClock className='text-gray-400' />
 					<span>Time Left</span>
-
 					<span className='countdown font-mono text-lg' aria-live='polite'>
 						{timeLeftState}
 					</span>
